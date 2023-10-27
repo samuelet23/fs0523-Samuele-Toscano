882fd23d5e90d78b2805c8788cf29c28484b96e6
samuelet23 <samueletoscano@hotmail.com>
2023-10-27 16:17:47 +0200

    effetuato alcune modifiche al main

--- mese-2/settimana-1/progetto settimanale/js/script.js
+++ mese-2/settimana-1/progetto settimanale/js/script.js
@@ -1,6 +1,9 @@
 let navBar = document.getElementById('nav-bar');
 let getStarted = document.getElementById('get-started');
 
+
+// superati i 500px di scroll dall'inizio della pagina  cambia il background, 
+// tornato ai500px ritorna come prima
 window.addEventListener('scroll', () => {
     let top = window.scrollY;
     if (top > 500) {
@@ -14,4 +17,43 @@ window.addEventListener('scroll', () => {
         navBar.style.backgroundColor = '';
         getStarted.style.backgroundColor = '';
     }
+});
+
+
+//  attiva il pulsante get strated 
+getStarted.addEventListener('click',()=>{
+    Swal.fire(
+        'Avrei creato un pop-up personalizzato',
+        'Ma non ho avuto il tempo',
+        'Sarà per la prossima volta'
+      )
+})
+
+
+// attiva il pulsante startreading
+let startReading= document.querySelector('.button-left-hero')
+
+startReading.addEventListener('click', ()=>{
+Swal.fire(
+  'Avrei creato un pop-up personalizzato',
+  'Ma non ho avuto il tempo',
+  'Sarà per la prossima volta'
+)
+})
+
+
+let miniProfiloPersona = document.querySelector('.profilopersona')
+let profiloPersona = document.querySelectorAll('.articoli')
+
+// al passagio del mouse attiva un pop up del profilo dell persona
+profiloPersona.forEach((elemento) => {
+    let miniProfiloPersona = elemento.querySelector('.profilopersona');
+
+    elemento.addEventListener('mouseover', () => {
+        miniProfiloPersona.style.display = 'block';
+    });
+
+    elemento.addEventListener('mouseout', () => {
+        miniProfiloPersona.style.display = 'none';
+    });
 });
