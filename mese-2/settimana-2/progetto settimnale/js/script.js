db3488dfbcc8c61342fabd386a0470edb692a045
samuelet23 <samueletoscano@hotmail.com>
2023-11-04 23:41:10 +0100

    carousel finish

--- mese-2/settimana-2/progetto settimnale/js/script.js
+++ mese-2/settimana-2/progetto settimnale/js/script.js
@@ -1,22 +1,23 @@
 let quadrati = document.querySelectorAll('.quadrato');
 let check = document.querySelectorAll('.check');
-const homePage= document.getElementById('page-1')
-const editPage= document.getElementById ('page-2')
-const profile = document.getElementById('profile')
-const logo= document.querySelector('.img-container')
-console.log(logo);
+const homePage= document.getElementById('page-1');
+const editPage= document.getElementById ('page-2');
+const profile = document.getElementById('profile');
 
 
 
 profile.addEventListener('click', () => {
-    homePage.style.display = 'none'
-    editPage.style.display = 'block'
+    homePage.style.visibility = 'none'
+    editPage.style.visibility = 'block'
 })
 
-logo.addEventListener('click', () =>{
-editPage.style.display = 'none'
-homePage.style.display = 'block'
-})
+
+
+
+
+
+
+
 
 
 
