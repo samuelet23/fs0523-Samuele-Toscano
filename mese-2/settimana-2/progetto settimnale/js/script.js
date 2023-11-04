05e84e0d7d81f8838c9f7f2dd2e10415cdc4d357
samuelet23 <samueletoscano@hotmail.com>
2023-11-05 00:11:40 +0100

    home page, edit page complete

--- mese-2/settimana-2/progetto settimnale/js/script.js
+++ mese-2/settimana-2/progetto settimnale/js/script.js
@@ -3,16 +3,28 @@ let check = document.querySelectorAll('.check');
 const homePage= document.getElementById('page-1');
 const editPage= document.getElementById ('page-2');
 const profile = document.getElementById('profile');
+const logoNetflix= document.querySelectorAll('#logo')
+const save= document.querySelector('.save');
+const cancel= document.querySelector('.cancel');
+const deleteProfile= document.querySelector('.delete-profile');
+
+
+
 
 
 
 profile.addEventListener('click', () => {
-    homePage.style.visibility = 'none'
-    editPage.style.visibility = 'block'
+    homePage.style.display = 'none'
+    editPage.style.display = 'block'
 })
 
+logoNetflix.forEach(logo =>{
+    logo.addEventListener('click', () => {
+        editPage.style.display='none'
+        homePage.style.display='block'
+    })
 
-
+})
 
 
 
@@ -25,7 +37,7 @@ profile.addEventListener('click', () => {
 quadrati.forEach(quadrato => {
     quadrato.addEventListener('click', () => {
         let oneCheck = quadrato.nextElementSibling;
-        oneCheck.classList.remove('invisible');
+        oneCheck.classList.toggle('hidden');
         
     });
 });
