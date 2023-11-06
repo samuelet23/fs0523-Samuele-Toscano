cc0df039a61b4ecd93c585ae4774a6b14824a853
samuelet23 <samueletoscano@hotmail.com>
2023-11-06 08:54:14 +0100

    aggiunti commenti

--- mese-2/settimana-2/progetto settimnale/js/script.js
+++ mese-2/settimana-2/progetto settimnale/js/script.js
@@ -1,24 +1,23 @@
-let quadrati = document.querySelectorAll('.quadrato');
-let check = document.querySelectorAll('.check');
-const homePage= document.getElementById('page-1');
-const editPage= document.getElementById ('page-2');
-const profile = document.getElementById('profile');
-const logoNetflix= document.querySelectorAll('#logo')
-const save= document.querySelector('.save');
-const cancel= document.querySelector('.cancel');
-const deleteProfile= document.querySelector('.delete-profile');
-const popUpAvatar = document.querySelector('.pop-up')
-const avatar = document.getElementById ('avatar');
-const buttonPopUp= document.querySelector('.button-popUp')
-const nav = document.getElementById('nav-avatar')
+let quadrati = document.querySelectorAll('.quadrato'); // quadrato checkbox
+let check = document.querySelectorAll('.check'); //checkbox verified 
+const homePage= document.getElementById('page-1'); //home page
+const editPage= document.getElementById ('page-2'); //edit page 
+const profile = document.getElementById('profile'); //immagine avatar
+const logoNetflix= document.querySelectorAll('#logo') //logo netflix 
+const save= document.querySelector('.save'); //button save
+const cancel= document.querySelector('.cancel'); //button cancel 
+const deleteProfile= document.querySelector('.delete-profile'); //button delete profile
+const popUpAvatar = document.querySelector('.pop-up') //pop up al click sull frecccia in basso 
+const avatar = document.getElementById ('avatar'); //freccia in giu accanto avatar
+const buttonPopUp= document.querySelector('.button-popUp') //bottone del pop up
 popUpAvatar.style.display='none';
 
+
+// per passare dalla home page alla edit page 
 profile.addEventListener('click', () => {
     homePage.style.display = 'none'
     editPage.style.display = 'block'
 })
-
-
 
 
 // funzione per avviare un pop, solo a scopo didattico per far vedere dove trovare la Edit Page
@@ -38,21 +37,6 @@ function avatarInteracctive() {
 }
 avatarInteracctive();
 
-function openPopupAfterDelay() {
-    setTimeout(function() {
-      let popup = document.createElement('div');
-      popup.textContent = "clicca l'avatar per passare alla Edit Page";
-     popup.classList.add('pop-up-delay')
-      document.body.appendChild(popup);
-  
-      // per chiudere il popup 
-      setTimeout(function() {
-        document.body.removeChild(popup);
-      }, 3000);
-    }, 6000); 
-  }
-  openPopupAfterDelay();
-  
 
 
 
@@ -84,3 +68,19 @@ function checkbox() {
 }
 checkbox()
 
+
+        function openPopupAfterDelay() {
+            setTimeout(function() {
+              let popup = document.createElement('div');
+              popup.textContent = "clicca l'avatar per passare alla Edit Page";
+             popup.classList.add('pop-up-delay')
+              document.body.appendChild(popup);
+          
+              // per chiudere il popup 
+              setTimeout(function() {
+                document.body.removeChild(popup);
+              }, 3000);
+            }, 6000); 
+          }
+          openPopupAfterDelay();
+          
