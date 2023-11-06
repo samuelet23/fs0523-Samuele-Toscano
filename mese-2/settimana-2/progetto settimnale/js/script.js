ae1362bec0dd17ae75a41737086ba4f1a5e5472e
samuelet23 <samueletoscano@hotmail.com>
2023-11-06 08:49:25 +0100

    Progetto settimanale s6 completo

--- mese-2/settimana-2/progetto settimnale/js/script.js
+++ mese-2/settimana-2/progetto settimnale/js/script.js
@@ -7,39 +7,80 @@ const logoNetflix= document.querySelectorAll('#logo')
 const save= document.querySelector('.save');
 const cancel= document.querySelector('.cancel');
 const deleteProfile= document.querySelector('.delete-profile');
-
-
-
-
-
+const popUpAvatar = document.querySelector('.pop-up')
+const avatar = document.getElementById ('avatar');
+const buttonPopUp= document.querySelector('.button-popUp')
+const nav = document.getElementById('nav-avatar')
+popUpAvatar.style.display='none';
 
 profile.addEventListener('click', () => {
     homePage.style.display = 'none'
     editPage.style.display = 'block'
 })
 
-logoNetflix.forEach(logo =>{
-    logo.addEventListener('click', () => {
-        editPage.style.display='none'
-        homePage.style.display='block'
+
+
+
+// funzione per avviare un pop, solo a scopo didattico per far vedere dove trovare la Edit Page
+function avatarInteracctive() {
+    avatar.addEventListener('click', () => {
+        if (popUpAvatar.style.display === 'block') {
+            popUpAvatar.style.display = 'none';
+        } else {
+            popUpAvatar.style.display = 'block';
+        }
+        
+        buttonPopUp.addEventListener('click', () => {
+            homePage.style.display = 'none';
+            editPage.style.display = 'block';
+        });
+    }); 
+}
+avatarInteracctive();
+
+function openPopupAfterDelay() {
+    setTimeout(function() {
+      let popup = document.createElement('div');
+      popup.textContent = "clicca l'avatar per passare alla Edit Page";
+     popup.classList.add('pop-up-delay')
+      document.body.appendChild(popup);
+  
+      // per chiudere il popup 
+      setTimeout(function() {
+        document.body.removeChild(popup);
+      }, 3000);
+    }, 6000); 
+  }
+  openPopupAfterDelay();
+  
+
+
+
+
+
+// funzione per ritornare all'home page al click sul logo
+function logoHomePage() {
+    logoNetflix.forEach(logo =>{
+        logo.addEventListener('click', () => {
+            editPage.style.display='none'
+            homePage.style.display='block'
+        })
+    
     })
-
-})
-
-
-
-
-
-
-
+    
+}
+logoHomePage()
 
 // funzione al click check the checkbox
-quadrati.forEach(quadrato => {
-    quadrato.addEventListener('click', () => {
-        let oneCheck = quadrato.nextElementSibling;
-        oneCheck.classList.toggle('hidden');
-        
+function checkbox() {
+    quadrati.forEach(quadrato => {
+        quadrato.addEventListener('click', () => {
+            let oneCheck = quadrato.nextElementSibling;
+            oneCheck.classList.toggle('hidden');
+            
+        });
     });
-});
-
+    
+}
+checkbox()
 
