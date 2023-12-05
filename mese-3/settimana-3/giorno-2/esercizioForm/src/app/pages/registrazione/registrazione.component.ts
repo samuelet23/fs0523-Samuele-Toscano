210bbbf541814448216cb104c649495a32bcef47
samuelet23 <samueletoscano@hotmail.com>
2023-12-05 16:50:02 +0100

    form registrazione

--- mese-3/settimana-3/giorno-2/esercizioForm/src/app/pages/registrazione/registrazione.component.ts
+++ mese-3/settimana-3/giorno-2/esercizioForm/src/app/pages/registrazione/registrazione.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, NgModel, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgModel, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrazione',
@@ -8,7 +8,7 @@ import { FormBuilder, FormGroup, NgModel, Validators } from '@angular/forms';
 })
 export class RegistrazioneComponent {
 
-
+  urlRegex: string = '/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/';
   form!:FormGroup
 
   constructor(private formBuuilder:FormBuilder){}
@@ -20,7 +20,7 @@ export class RegistrazioneComponent {
       password: this.formBuuilder.control(null,[Validators.required]),
       confermaPassword: this.formBuuilder.control(null,[Validators.required]),
       genere: this.formBuuilder.control(null,[Validators.required]),
-      immagineProfilo: this.formBuuilder.control(null,[Validators.required]),
+      immagineProfilo: this.formBuuilder.control(null,[Validators.required,this.urlImageCheck]),
       biografia: this.formBuuilder.control(null,[Validators.required]),
       username: this.formBuuilder.control(null,[Validators.required])
 
@@ -28,6 +28,23 @@ export class RegistrazioneComponent {
     })
 
   }
+
+
+  getCustomMessage(fieldName: string): string {
+    const errors = this.form.get(fieldName)?.errors;
+    return errors ? errors['message'] : '';
+  }
+
+  urlImageCheck = (input: FormControl): ValidationErrors | null => {
+    if (!new RegExp(this.urlRegex).test(input.value)) {
+      return {
+        invalid: true,
+        message: 'Non è un URL corretto'
+      };
+    }
+    return null;
+  };
+
 
   isValid(name:string){
     return this.form.get(name)?.valid
@@ -38,6 +55,27 @@ export class RegistrazioneComponent {
   }
 
   isBoth(name:string){
-    return this.isValid(name) && this.isTouched(name)
+    return !this.isValid(name) && this.isTouched(name)
   }
-}
+  iscriviti(form:FormGroup){
+    if(form.valid){
+      return alert('Ti sei registrato con successo')
+    }
+    return alert('Compila tutti i campi')
+  }
+
+  isSame(passwordValue: string, secondoPasswordValue: string) {
+    if (passwordValue === secondoPasswordValue) {
+      return true;
+    }
+
+    return false;
+  }
+
+
+
+
+
+  }
+
+
